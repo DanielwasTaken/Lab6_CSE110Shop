f32dc47e805a6ea4fd0bd36aa7cf09081a90b3c2
DanielwasTaken <53798206+DanielwasTaken@users.noreply.github.com>
2021-02-11 07:16:14 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,11 @@
 
 window.addEventListener('DOMContentLoaded', () => {
   var response = getProducts("https://fakestoreapi.com/products");
+  
 });
   
 async function getProducts(file){
-  let rsp = await fetch(file);
-  let response = JSON.stringify(rsp.json());
-  alert(response);
+  let rsp = await fetch(file).json();
+  let response = JSON.stringify(rsp);
   return response;
 }
