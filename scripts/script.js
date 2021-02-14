806aec142ab87d3d4634f1a8c282b631c524e47e
DanielwasTaken <53798206+DanielwasTaken@users.noreply.github.com>
2021-02-13 18:52:49 -0800

    added local storage

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,15 @@
 // Script.js
 
 window.addEventListener('DOMContentLoaded', () => {
-  var response = getProducts("https://fakestoreapi.com/products");
+  var lStorage = window.localStorage;
+  if(lStorage == null){
+    var response = getProducts("https://fakestoreapi.com/products");
+    localStorage.setItem("products", response);
+  }
+  else{
+    //make sure to delete later
+    alert("local storage not empty");
+  }
   
 });
   
